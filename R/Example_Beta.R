3a4b4a8d0766d0eded2f552c3493395c912a4753
Ze Zhang <ze.zhang.gr@dartmouth.edu>
2023-03-31 19:08:01 -0400

    Update0331

--- R/Example_Beta.R
+++ R/Example_Beta.R
@@ -1,11 +1,11 @@
 #' Example Beta to test for HiTIMED
 #'
 #' @description
-#'     This object contains a matrix of beta values from three colon 
-#'     adenocarcinoma samples 6285625076_R06C02", "6929718079_R01C01", 
-#'     "6042316015_R02C02". These samples are from publicly available 
+#'     This object contains a matrix of beta values from three colon
+#'     adenocarcinoma samples "6285625076_R06C02", "6929718079_R01C01",
+#'     "6042316015_R02C02". These samples are from publicly available
 #'     data source TCGA at https://portal.gdc.cancer.gov/.
-#'     
+#'
 #'
 #' @format The matrix contains methylation beta values
 #'
